e83cd3077dd1a4138c7f4c58f45223d864f0c3d7
Brody <bjgraham@wpi.edu>
2025-06-12 22:39:13 -0400

    Reworking Rigidbody based glider

--- airdrifter-v1/Gliders/PhysicsGlider.gd
+++ airdrifter-v1/Gliders/PhysicsGlider.gd
@@ -13,17 +13,36 @@ func _ready() -> void:
 	
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	
+	# Current Velocity: Linear Velocity has the aspects of Initial Speed and direction, Lift, and Gravity 
+	var current_velo = state.linear_velocity
+	
+	# Unit vector of where the glider is going
+	forward_vector = (global_transform.basis * Vector3(0, 0, 1)).normalized()
+	
+	var new_velo : Vector3
+	
+	#Changing the direction of the current velocity to go where the glider is pointing
+	new_velo = forward_vector * current_velo.length()
+	new_velo.y += current_velo.y
+	
+	#TODO: Implement drag to prevent the glider from accelerating out of control
+	#TODO: Implement lift 
+	
+	state.linear_velocity = new_velo
+	
 	# Adding Lift (effected by horizontal velocity)
-	state.linear_velocity += calculate_lift(state)
-	
-	var vertical_velocity = linear_velocity.y
-	
-	#Problem: When we rotate left and right, the plane doesn't turn immediately. That only changes as a result of losing altitude
-	if vertical_velocity < 0:
-		forward_vector = (global_transform.basis * Vector3(0, 0, 1)).normalized()
-		forward_vector *= vertical_velocity * -0.1
-		state.linear_velocity += forward_vector
-	
+	#state.linear_velocity.y += calculate_lift(state).y
+	#
+	#var vertical_velocity = linear_velocity.y
+	#
+	##Problem: When we rotate left and right, the plane doesn't turn immediately. That only changes as a result of losing altitude
+	##if vertical_velocity < 0:
+	#forward_vector = (global_transform.basis * Vector3(0, 0, 1)).normalized()
+	#forward_vector *= vertical_velocity * -0.1
+	#forward_vector.clamp(Vector3(-INF, 0, -INF), Vector3.INF)
+	#state.linear_velocity += forward_vector
+		
 	
 
 func calculate_lift(state: PhysicsDirectBodyState3D) -> Vector3:
@@ -31,7 +50,7 @@ func calculate_lift(state: PhysicsDirectBodyState3D) -> Vector3:
 	var up_vector = (global_transform.basis * Vector3(0, 1, 0)).normalized()
 	# Finding Horizontal (X and Z) components of linear velocity
 	var horizontal_velocity = state.linear_velocity
-	horizontal_velocity.y = 0.0
+	#horizontal_velocity.y = 0.0
 	
 	# Calculating lift based on horizontal velocity
 	var lift = up_vector *  horizontal_velocity.length() * 0.01
@@ -49,3 +68,5 @@ func _physics_process(delta: float) -> void:
 		rotate(Vector3.MODEL_TOP, deg_to_rad(rotation_rate * delta ))
 	if Input.is_action_pressed("bank_right"):
 		rotate(Vector3.MODEL_TOP, deg_to_rad(rotation_rate * delta * -1.0))
+	
+	#translate(Vector3(0, 0, 10 * delta))
