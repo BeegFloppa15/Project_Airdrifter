40e61f306a185f7c2a01b33ba12c42042a615b48
Brody <bjgraham@wpi.edu>
2025-07-08 16:04:36 -0400

    Reworked Elytra movement, requires more tweaking, but new script seems more stable and scalable

--- airdrifter-v1/Gliders/ElytraGlider2.gd
+++ airdrifter-v1/Gliders/ElytraGlider2.gd
@@ -3,9 +3,10 @@ class_name ElytraGlider2
 
 ## If checked off, the scene will be displayed through the attatched camera
 @export var use_this_cam: bool = false
-@export var rotation_torque: float = 400
+@export var rotation_torque: float = 200
 #@export var angle_acceleration_curve: Curve
 var forward_dir: Vector3
+var old_forward_velo: Vector3
 @export var forward_velocity: Vector3
 @export var acceleration : float = 0.0
 @export var minimum_speed : float = 50
@@ -15,72 +16,43 @@ func _ready() -> void:
 		$"3rdPerCamera".make_current()
 	else:
 		$"3rdPerCamera".queue_free()
-
-func _physics_process(delta: float) -> void:
-	## Forward direction of travel
-	#var forward_local_axis: Vector3 = Vector3(0, 0, 1)
-	#forward_dir = (global_transform.basis * forward_local_axis).normalized()
-	#var delta_pitch = rotation.x * -1.0
-	#var delta_yaw = rotation.y * -1.0
-	#
-	## Measuring Forward Velocity
-	#forward_velocity = linear_velocity.project(forward_dir)
-	#
-	##Change the roll of the glider, and UPDATING forward direction
-	handle_input()
-	#forward_dir = (global_transform.basis * forward_local_axis).normalized()
-	#delta_pitch += rotation.x
-	#delta_yaw += rotation.y
-	#print(delta_pitch)
-	#print(delta_yaw)
-	#
-	## Determining Acceleration value based on pitch
-	##dot > 0 means glider pointed down
-	##dot < 0 means glider pointed up
-	#acceleration = Vector3.UP.dot(forward_dir) * -10
-	#var acceleration_vector = Vector3(forward_dir) * acceleration
 	
-	# Chaning the direction of linear velocity 
-	#state.linear_velocity -= forward_velocity
-	#var new_forward_velocity = Vector3(forward_dir) * forward_velocity.length()
-	#new_forward_velocity += acceleration_vector
-	#state.linear_velocity += new_forward_velocity
-	#
-	#state.apply_central_force(find_lift(new_forward_velocity))
-
+	old_forward_velo = Vector3.ZERO
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	# Forward direction of travel
+	
+	state.linear_velocity -= old_forward_velo
+	
+	# Pitch and rotate glider
+	handle_input()
+	
+	# Finding forward DIRECTION (unit Vector)
 	var forward_local_axis: Vector3 = Vector3(0, 0, 1)
 	forward_dir = (global_transform.basis * forward_local_axis).normalized()
-	var delta_pitch = rotation.x * -1.0
-	var delta_yaw = rotation.y * -1.0
 	
-	# Measuring Forward Velocity
-	forward_velocity = state.linear_velocity.project(forward_dir)
-	
-	#Change the roll of the glider, and UPDATING forward direction
-	#handle_input(state)
-	forward_dir = (global_transform.basis * forward_local_axis).normalized()
-	
-	
-	# Determining Acceleration value based on pitch
+	# dot product between the forward direction and Vector3.UP: 
 	#dot > 0 means glider pointed down
 	#dot < 0 means glider pointed up
-	acceleration = Vector3.UP.dot(forward_dir) * -10
-	var acceleration_vector = Vector3(forward_dir) * acceleration
+	acceleration = Vector3.UP.dot(forward_dir) * -2
 	
-	# Chaning the direction of linear velocity 
-	state.linear_velocity -= forward_velocity
-	var new_forward_velocity = Vector3(forward_dir) * forward_velocity.length()
-	new_forward_velocity += acceleration_vector
-	state.linear_velocity += new_forward_velocity
+	forward_velocity = forward_dir * (old_forward_velo.length() + acceleration)
 	
-	state.apply_central_force(find_lift(new_forward_velocity))
+	state.linear_velocity += forward_velocity
+	
+	state.apply_central_force(find_lift(forward_velocity))
+	
+	old_forward_velo = state.linear_velocity.project(forward_dir)
+
+func _physics_process(delta: float) -> void:
+	#Clamping values to stop unwanted rotational behavior (my silly billy ahh ahh keeps forgetting
+	# to do deg_to_rad
+	rotation = rotation.clamp(Vector3(deg_to_rad(-85.0), -10, 0.0), Vector3(deg_to_rad(85.0), 10, 0.0))
 
 func find_lift(forward_velocity: Vector3):
 	var max_lift = 30.0;
 	var up_dir = forward_velocity.normalized().rotated((global_transform.basis * Vector3(-1, 0, 0)).normalized(), deg_to_rad(90))
+	#up_dir.x = 0
+	#up_dir.z = 0
 	var lift_force = min(forward_velocity.length() * 0.7, max_lift) 
 	return up_dir * lift_force
 
