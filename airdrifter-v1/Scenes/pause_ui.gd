a4fa995aa2abd0e61f2c01b3d9aca70f8285e477
Brody <bjgraham@wpi.edu>
2025-07-28 14:46:40 -0400

    Added Basic Pause Menu with Controls

---
+++ airdrifter-v1/Scenes/pause_ui.gd
@@ -0,0 +1,46 @@
+extends Control
+
+@export var air_controls_img: Texture
+@export var ground_controls_img: Texture
+var paused: bool
+
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+func _on_controls_toggle_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$ControlsImg.texture = ground_controls_img
+	else:
+		$ControlsImg.texture = air_controls_img
+
+
+func pause_game():
+	self.show()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_tree().paused = true
+	paused = true
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("pause"):
+		if not paused:
+			pause_game()
+		else:
+			resume_game()
+
+
+func resume_game() -> void:
+	self.hide()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().paused = false
+	paused = false
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
